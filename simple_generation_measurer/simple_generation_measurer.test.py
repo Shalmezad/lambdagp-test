67729f23a747e29b838e7500750ef366633a84e1
Richard Wardin <shalmezad@gmail.com>
2024-07-05 17:09:42 -0400

    Fixed simple_generation_builder and simple_regression_problem to work with python3.10

--- simple_generation_measurer/simple_generation_measurer.test.py
+++ simple_generation_measurer/simple_generation_measurer.test.py
@@ -31,6 +31,5 @@ class TestHandleGenerationRunnerEvent(unittest.TestCase):
         self.assertEqual(result_meta["fitnesses"], {})
 
 
-
 if __name__ == '__main__':
     unittest.main()
--- simple_regression_problem/component_types.py
+++ simple_regression_problem/component_types.py
@@ -1,4 +1,8 @@
-from typing import List, TypedDict, Any, Dict, NotRequired
+import sys
+if sys.version_info < (3, 11):
+    from typing_extensions import TypedDict, Any, Dict, NotRequired, List
+else:
+    from typing import TypedDict, Any, Dict, NotRequired, List
 
 
 class SimpleRegressionProblemEvent(TypedDict):
